529f107f3c0350f3751994d816a09b3f05564b85
harsh3sahu <103841259+harsh3sahu@users.noreply.github.com>
2026-05-31 15:43:54 +0530

    Add semantic retriever

---
+++ app/rag/retriever.py
@@ -0,0 +1,56 @@
+from app.vectorstore.vector_store import vector_store
+from app.rag.embedding_manager import embedding_manager
+
+class Retriever:
+    def __init__(self):
+        self.vector_store=vector_store
+
+        self.embedding_manager=embedding_manager
+
+
+    def retrieve(self,query:str,top_k:int=5,score_threshold:float=0.2):
+
+        print(f"\n Retrieving for query: {query}")
+
+        query_embedding= self.embedding_manager.generate_embeddings([query])[0]
+
+        results= (
+            self.vector_store.collection.query(
+                query_embeddings=[
+                    query_embedding.tolist()
+                ],
+                n_results=top_k
+            )
+        )
+
+        retrieved_docs=[]
+
+        if results["documents"] and results["documents"][0]:
+
+            documents=results["documents"][0]
+
+            metadatas= results["metadatas"][0]
+
+            distances=results["distances"][0]
+
+            ids=results["ids"][0]
+
+            for i,(doc_id,document,metadata,distance) in enumerate (zip(ids,documents,metadatas,distances)):
+                similarity_score=max(0.0, 1.0- (distance/2.0))
+
+                if (similarity_score>=score_threshold):
+                    retrieved_docs.append({
+                        "id":doc_id,
+                        "content":document,
+                        "metadata":metadata,
+                        "similarity_score":similarity_score,
+                        "distance":distance,
+                        "rank":i+1
+                    })
+            
+            
+        return retrieved_docs
+
+
+retriever = Retriever()
+        

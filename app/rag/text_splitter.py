026a803af35270af547fd280bb511db50daa31a4
harsh3sahu <103841259+harsh3sahu@users.noreply.github.com>
2026-05-31 12:55:46 +0530

    Add document chunking pipeline

---
+++ app/rag/text_splitter.py
@@ -0,0 +1,43 @@
+from langchain_core.language_models._compat_bridge import chunks_to_events
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+
+from app.config import Config
+
+class TextSplitter:
+
+    def __init__(self):
+        self.splitter=(
+            RecursiveCharacterTextSplitter(
+            chunk_size=Config.CHUNK_SIZE,
+            chunk_overlap=Config.CHUNK_OVERLAP
+            )
+        )
+
+
+    def split_documents(self,documents:list):
+        chunks=[]
+
+        for document in documents:
+            content=document["content"]
+
+            metadata= document["metadata"]
+
+            split_texts=(
+                self.splitter.split_text(
+                    content
+                )
+            ) 
+            for chunk_id, chunk in enumerate(split_texts):
+                chunks.append(
+                    {
+                        "content":chunk,
+                        "metadata":{
+                            **metadata,
+                            "chunk_id":chunk_id
+                        }
+                    }
+                )
+
+        return chunks
+
+text_splitter=TextSplitter()
